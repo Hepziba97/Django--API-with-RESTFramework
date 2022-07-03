d62c20ca5c7a95c657c596eb2acc0c6ba35d5e8e
HEPHZIBAH FRANCIS <franciumhosiad@gmail.com>
2022-07-03 03:38:49 +0100

    working with APIs

--- I4G006008Z1D/links/views.py
+++ I4G006008Z1D/links/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Link
+from .serializers import LinkSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.throttling import UserRateThrottle
+
+ 
 
 # Create your views here.
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateApi(CreateAPIView):
+    permission_classes = [IsAdminUser]
+
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+    
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(DestroyAPIView):
+    queryset= Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        links = Link.objects.all()
+        serializer = LinkSerializer(links, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = LinkSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Link.objects.get(pk=pk)
+    except Link.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = LinkSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LinkSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
